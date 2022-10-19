bb4ed1bd4d845d57282eec668881049f70b45294
Arne Wouters <arne@wouters.co>
2022-10-19 13:03:37 +0200

    Add code for Ch. 5 - Adding a Sphere

--- Rust/src/vec.rs
+++ Rust/src/vec.rs
@@ -145,7 +145,7 @@ impl Vec3 {
         self.length_squared().sqrt()
     }
 
-    pub fn dot(a: &Vec3, b: &Vec3) -> f64 {
+    pub fn dot(a: Vec3, b: Vec3) -> f64 {
         (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
     }
 
